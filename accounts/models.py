b96e4606b4dc6f9175a66b7ec2be9dcf6c9a0ed7
57381647+KienPS <57381647+KienPS@users.noreply.github.com>
2024-09-04 16:59:46 +0700

    Fix columns not found

--- accounts/models.py
+++ accounts/models.py
@@ -9,7 +9,7 @@ from job_recruitment.models import Company
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    phone_number = models.CharField(max_length=11, unique=True, blank=True)
+    phone_number = models.CharField(max_length=11, blank=True)
     address = models.CharField(blank=True, max_length=255)
     company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True, related_name='members')
     is_manager = models.BooleanField(default=False)
